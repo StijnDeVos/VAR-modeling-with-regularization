8afd0b82100af98cf11c9b3f54524b3d18b3d1c8
StijnDeVos <pillarsofmercy@hotmail.com>
2017-11-03 16:53:58 +0100

    started writing Regularization section

---
+++ regularization_example.R
@@ -0,0 +1,15 @@
+library(glmnet)
+
+x1 <- rnorm(100)
+x2 <- rnorm(100, mean = -1, sd = 3)*x1
+y <- 1 + x1 + 2*x2 + rnorm(100, sd = 2)
+data <- data.frame(x1 = x1, x2 = x2, y = y)
+pairs(data)
+
+simple_lm <- lm(y ~ x1 + x2)
+coefficients(simple_lm)
+
+X = matrix(NA, ncol = 2, nrow = 100)
+X[, 1] <- data$x1
+X[, 2] <- data$x2
+penalized_lm <- glmnet(x = X, y)
