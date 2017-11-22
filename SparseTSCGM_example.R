ab4a0c8367851dbb5aab6b521c50158083519255
StijnDeVos <pillarsofmercy@hotmail.com>
2017-11-22 17:33:12 +0100

    Added bibliography, finished second section, started third

---
+++ SparseTSCGM_example.R
@@ -0,0 +1,5 @@
+library(SparseTSCGM)
+example_data <- sim.data(model = "ar1", 
+                         time = 50,
+                         n.obs = 10,
+                         n.var = 14)
--- regularization_example.R
+++ regularization_example.R
@@ -1,17 +1,22 @@
 library(glmnet)
 
+# Not fake news, but fake data:
 x1 <- rnorm(100)
 x2 <- rnorm(100, mean = -1, sd = 3)*x1
 y <- 1 + x1 + 2*x2 + rnorm(100, sd = 2)
 data <- data.frame(x1 = x1, x2 = x2, y = y)
 pairs(data)
 
+# simple linear regression without regularization:
 simple_lm <- lm(y ~ x1 + x2)
 coefficients(simple_lm)
 
-X = matrix(NA, ncol = 2, nrow = 100)
-X[, 1] <- data$x1
-X[, 2] <- data$x2
-penalized_lm <- glmnet(x = X, y)
-cv <- cv.glmnet(x = X, y = y)
+# regression using elastic-net regularization:
+cv <- cv.glmnet(x = cbind(x1, x2), y = y)
 
+# This is the optimal value for lambda according to cross-validation:
+cv$lambda.min
+
+# fitting a penalized regression model using the optimal value for lambda:
+penalized_lm_opt <- glmnet(x = X, y, lambda = cv$lambda.min)
+coef(penalized_lm_opt)
